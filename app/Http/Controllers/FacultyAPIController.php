f9288d141979df589b127f9dbdf07e4c1f7c1c9e
6414421016 <117976265+6414421016@users.noreply.github.com>
2024-08-02 15:08:41 +0700

    Add files via upload

---
+++ app/Http/Controllers/FacultyAPIController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Faculty;
+use Illuminate\Http\Request;
+
+class FacultyAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $faculties = Faculty::all();
+        return response()->json($faculties);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'faculty_th' => 'required|string|max:255',
+            'faculty_en' => 'required|string|max:255',
+        ]);
+        $faculty = Faculty::create($validateData);
+        return response()->json($faculty, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $faculty = Faculty::find($id);
+        return response()->json($faculty, 201);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate([
+            'faculty_th' => 'required|string|max:255',
+            'faculty_en' => 'required|string|max:255',
+        ]);
+        $faculty = Faculty::find($id);
+        $faculty->update($validateData);
+        return response()->json($faculty, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $faculty = Faculty::find($id);
+        if(!$faculty) {
+            return response()->json(["Error" => 'Resource not found'], 404);
+        }
+        $faculty->delete();
+        return response()->json(null, 204);
+    }
+}
